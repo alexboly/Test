c99d25437321a7e78ac77b206a2684656ad84553
Thomas Sundberg <tsu@kth.se>
2011-05-18 21:59:48 +0200

    Added lots of tests and got it to work

--- WordWrap/src/com/WordWrap/WordWrapTests.java
+++ WordWrap/src/com/WordWrap/WordWrapTests.java
@@ -1,5 +1,6 @@
 package com.WordWrap;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.LinkedList;
@@ -11,69 +12,126 @@ import static org.junit.Assert.assertThat;
 
 public class WordWrapTests {
 
-	@Test
-	public void rowShorterThanRowLenghtDoesntGetWrapped() {
-		String initialRow = "abcdefg";
+    @Test
+    public void rowShorterThanRowLenghtDoesntGetWrapped() {
+        String initialRow = "abcdefg";
 
-		List<String> wrappedRow = wrapText(initialRow, 10);
+        List<String> wrappedRow = wrapText(initialRow, 10);
 
-		assertEquals("abcdefg", wrappedRow.get(0));
-	}
+        assertEquals("abcdefg", wrappedRow.get(0));
+    }
 
-	@Test
-	public void rowLongerThanWantedRowLength() {
-		String initialRow = "abc def";
-		int wantedRowLength = 4;
+    @Test
+    public void rowLongerThanWantedRowLength() {
+        String initialRow = "abc def";
+        int wantedRowLength = 4;
 
-		List<String> expectedText = new LinkedList<String>();
-		expectedText.add("abc");
-		expectedText.add("def");
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
 
-		List<String> actualText = wrapText(initialRow, wantedRowLength);
-		assertThat(actualText, is(expectedText));
-	}
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
 
-	@Test
-	public void textGetsWrappedOnThreeLines() {
-		String initialRow = "abcdef abc";
-		int wantedRowLength = 3;
+    @Test
+    public void getTextWrappedOnTwoRows() {
+        String initialRow = "abc def";
+        int wantedRowLength = 3;
 
-		List<String> expectedText = new LinkedList<String>();
-		expectedText.add("abc");
-		expectedText.add("def");
-		expectedText.add("abc");
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
 
-		List<String> actualText = wrapText(initialRow, wantedRowLength);
-		assertThat(actualText, is(expectedText));
-	}
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
 
-	private List<String> wrapText(String text, int wantedRowLength) {
-		List<String> rows = new LinkedList<String>();
+    @Test
+    public void getTextWrappedOnThreeRowsWithSpaces() {
+        String initialRow = "a b c d e f";
+        int wantedRowLength = 3;
 
-		if (text.length() < wantedRowLength) {
-			rows.add(text);
-		} else if (text.length() < 2 * wantedRowLength) {
-			int startPosition = 0;
-			int endPosition = wantedRowLength;
-			while (startPosition < text.length()) {
-				rows.add(substring(text, startPosition, endPosition));
-				startPosition += wantedRowLength;
-				if (endPosition + wantedRowLength > text.length()) {
-					endPosition = text.length();
-				} else {
-					endPosition += wantedRowLength;
-				}
-			}
-		} else {
-			rows.add(substring(text, 0, wantedRowLength));
-			rows.add(substring(text, wantedRowLength, 2 * wantedRowLength));
-			rows.add(substring(text, 2 * wantedRowLength, text.length()));
-		}
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("a b");
+        expectedText.add("c d");
+        expectedText.add("e f");
 
-		return rows;
-	}
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
 
-	private String substring(String text, int startPosition, int endPosition) {
-		return text.substring(startPosition, endPosition).trim();
-	}
+    @Test
+    public void textGetsWrappedOnThreeLines() {
+        String initialRow = "abcdef abc";
+        int wantedRowLength = 3;
+
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
+        expectedText.add("abc");
+
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
+
+    @Test
+    public void textGetsWrappedOnThreeLinesWithoutAnySpace() {
+        String initialRow = "abcdefabc";
+        int wantedRowLength = 3;
+
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
+        expectedText.add("abc");
+
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
+
+    @Test
+    public void textGetsWrappedOnFourLinesWithoutAnySpace() {
+        String initialRow = "abcdefabcxyz";
+        int wantedRowLength = 3;
+
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
+        expectedText.add("abc");
+        expectedText.add("xyz");
+
+        List<String> actualText = wrapText(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
+
+    private List<String> wrapText(String text, int wantedRowLength) {
+        List<String> rows = new LinkedList<String>();
+
+        if (text.length() < wantedRowLength) {
+            rows.add(text);
+        } else {
+            int startPosition = 0;
+            int endPosition = wantedRowLength;
+            while (startPosition < text.length()) {
+                if (text.substring(startPosition).startsWith(" ")) {
+                    startPosition++;
+                    endPosition++;
+                }
+
+                rows.add(substring(text, startPosition, endPosition));
+
+                startPosition += wantedRowLength;
+                endPosition += wantedRowLength;
+                if (endPosition > (text.length())) {
+                    endPosition = text.length();
+                }
+            }
+        }
+
+        return rows;
+    }
+
+    private String substring(String text, int startPosition, int endPosition) {
+        return text.substring(startPosition, endPosition).trim();
+    }
 }

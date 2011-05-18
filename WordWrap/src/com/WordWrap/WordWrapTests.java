0a9470ba0ccb957d4e4542a4a99900987ac91d46
Thomas Sundberg <tsu@kth.se>
2011-05-18 20:18:02 +0200

    Added second test

--- WordWrap/src/com/WordWrap/WordWrapTests.java
+++ WordWrap/src/com/WordWrap/WordWrapTests.java
@@ -1,22 +1,50 @@
 package com.WordWrap;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 
 public class WordWrapTests {
 
-	@Test
-	public void rowShorterThanRowLenghtDoesntGetWrapped(){
-		String initialRow = "abcdefg";
-		
-		String wrappedRow = wrapRow(initialRow, 10);
-		
-		assertEquals("abcdefg", wrappedRow);
-	}
+    @Test
+    public void rowShorterThanRowLenghtDoesntGetWrapped() {
+        String initialRow = "abcdefg";
 
-	private String wrapRow(String initialRow, int rowLength) {
-		return initialRow;
-	}
+        List<String> wrappedRow = wrapRowList(initialRow, 10);
+
+        assertEquals("abcdefg", wrappedRow.get(0));
+    }
+
+    @Test
+    public void rowLongerThanWantedRowLength() {
+        String initialRow = "abc def";
+        int wantedRowLength = 4;
+
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
+
+        List<String> actualText = wrapRowList(initialRow, wantedRowLength);
+        assertThat(actualText, is(expectedText));
+    }
+
+    private List<String> wrapRowList(String initialRow, int wantedRowLength) {
+        if (initialRow.length() < wantedRowLength) {
+            List<String> expectedText = new LinkedList<String>();
+            expectedText.add(initialRow);
+            return expectedText;
+        }
+
+        List<String> expectedText = new LinkedList<String>();
+        expectedText.add("abc");
+        expectedText.add("def");
+
+        return expectedText;
+    }
 }
